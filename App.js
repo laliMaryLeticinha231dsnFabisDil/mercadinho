c00b50ac0fb5b2fdfa9edec5d5389245b302354d
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-05-10 16:53:50 -0300

    ok

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Animated, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [fadeAnim] = useState(new Animated.Value(0));
+
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.Image
+        source={require('./assets/dia-logo.png')}
+        style={[styles.image, { opacity: fadeAnim }]}
+      />
+      <Button title="Fade In" onPress={fadeIn} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
   },
 });
+
+export default App;
